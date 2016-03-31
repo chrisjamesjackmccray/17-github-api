6ba4f4c0e53ecb9451deb9797aa0abb1721a674e
James McCray <chrisjamesjackmccray@gmail.com>
2016-03-31 13:56:39 -0500

    Assignment

--- app/app.js
+++ app/app.js
@@ -1,2 +1,12 @@
 import 'es6-promise';
 import 'whatwg-fetch';
+
+fetch(`https://api.github.com/users/jbarket`)
+.then((response) => {
+  return response.json();
+
+})
+
+.then((response) => {
+  console.log(response);
+})
