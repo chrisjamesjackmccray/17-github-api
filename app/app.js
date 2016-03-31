2848333e5eff9b2f5af651e45ec4d7d8f64c5354
James McCray <chrisjamesjackmccray@gmail.com>
2016-03-31 16:15:02 -0500

    Assignment Commit

--- app/app.js
+++ app/app.js
@@ -6,7 +6,32 @@ fetch(`https://api.github.com/users/jbarket`)
   return response.json();
 
 })
-
 .then((response) => {
   console.log(response);
+  document.querySelector("#name").textContent = response.name;
+})
+
+fetch(`https://api.github.com/users/jbarket/repos`)
+.then((response) => {
+  return response.json();
+})
+
+.then((response) => {
+  // console.log(response);
+
+  let repoDiv = document.querySelector("#lowerdiv");
+
+  let img = document.createElement("img");
+
+
+  response.forEach((repo) => {
+    console.log(repo);
+
+    let div = document.createElement("div");
+    div.innerHTML = repo.name;
+
+    repoDiv.appendChild(div);
+
+  })
+
 })
