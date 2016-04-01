7e2c92fc6d62323432d20868ce5d53921c9e9909
James McCray <chrisjamesjackmccray@gmail.com>
2016-04-01 15:26:44 -0500

    Commit

--- app/app.js
+++ app/app.js
@@ -1,37 +1,75 @@
 import 'es6-promise';
 import 'whatwg-fetch';
 
-fetch(`https://api.github.com/users/jbarket`)
-.then((response) => {
-  return response.json();
+fetch('https://api.github.com/users/jbarket')
+  .then((response) => {
+    console.log(response);
+    return response.json();
+  })
+  .then((response) => {
+    console.log(response);
 
-})
-.then((response) => {
-  console.log(response);
-  document.querySelector("#name").textContent = response.name;
-})
+    document.querySelector("#avatar").src = response.avatar_url;
+    document.querySelector("#name").textContent = response.name;
+    document.querySelector("#username").textContent = response.login;
+    document.querySelector("#location").textContent = response.location;
+    document.querySelector("#email").textContent = response.email;
 
-fetch(`https://api.github.com/users/jbarket/repos`)
-.then((response) => {
-  return response.json();
-})
+      let blog = document.querySelector("#blog");
+      blog.textContent = response.blog;
+      blog.href = response.blog;
 
-.then((response) => {
-  // console.log(response);
-
-  let repoDiv = document.querySelector("#lowerdiv");
-
-  let img = document.createElement("img");
-
-
-  response.forEach((repo) => {
-    console.log(repo);
-
-    let div = document.createElement("div");
-    div.innerHTML = repo.name;
-
-    repoDiv.appendChild(div);
-
+      let email = document.querySelector("#email");
+      email.textContent = response.email;
+      email.href = `mailto:${response.email}`;
   })
 
+
+
+
+
+
+  fetch('https://api.github.com/users/jbarket/repos')
+  .then((response) => {
+   return response.json();
 })
+
+.then((response) => {
+  console.log(response);
+
+  response.forEach((repo) => {
+
+    let row = document.createElement("div");
+    row.classList.add("repo");
+
+    let repoName = document.createElement("a");
+    repoName.textContent = repo.name;
+    repoName.href = repo.html_url;
+
+    let info = document.createElement("div");
+    info.classList.add("info");
+
+    let language = document.createTextNode(repo.language);
+    info.appendChild(language);
+
+    let star = document.createElement("i");
+    star.classList.add("fa");
+    star.classList.add("fa-star");
+    info.appendChild(star);
+
+   let fork = document.createElement("i");
+   fork.classList.add("fa");
+   fork.classList.add("fa-code-fork");
+   info.appendChild(star);
+
+    let stargazers = document.createTextNode(repo.stargazers_count);
+    info.appendChild(stargazers);
+
+
+    row.appendChild(repoName);
+    row.appendChild(info);
+
+    document.querySelector("#repos").appendChild(row);
+
+  });
+});
